39528b7e3532050c48462405973e735f8340e8e8
Daniel Nyong <danielnyong66@gmail.com>
2022-09-25 00:22:23 +0100

    User route tests refactoring

---
+++ server/src/tests/helpers/user.ts
@@ -0,0 +1,93 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { UserDataSchemaDB } from 'app-schema/users';
+import application from 'application';
+import { StatusCodes } from 'http-status-codes';
+import Joi from 'joi';
+import {
+	updateUser,
+	updateUserPassword,
+	users,
+} from 'tests/authentication/user-data';
+
+chai.use(chaiHttp).should();
+
+async function getUser() {
+	const userTokens: string[] = await users.getUserTokens();
+	userTokens.should.not.be.empty;
+	for (const userToken of userTokens) {
+		const response = await chai
+			.request(application)
+			.get('/api/v1/user')
+			.auth(userToken, { type: 'bearer' });
+		response.should.have.status(StatusCodes.OK);
+		response.body.should.be.an('object');
+		Joi.assert(response.body, UserDataSchemaDB);
+	}
+}
+
+async function patchUser() {
+	let n = 0;
+	const userTokens: string[] = await users.getUserTokens();
+	userTokens.should.not.be.empty;
+	for (const userToken of userTokens) {
+		const response = await chai
+			.request(application)
+			.patch('/api/v1/user/info')
+			.send(updateUser[n])
+			.auth(userToken, { type: 'bearer' });
+		response.should.have.status(StatusCodes.OK);
+		response.body.should.be.an('object');
+		Joi.assert(response.body, UserDataSchemaDB);
+		n++;
+	}
+}
+
+async function patchUserPassword() {
+	let n = 0;
+	const userTokens: string[] = await users.getUserTokens();
+	userTokens.should.not.be.empty;
+	// console.log(`\nusers: %O\n%s`, userTokens, __filename);
+	let lastPassword = {},
+		lastToken = '';
+	for (const userToken of userTokens) {
+		// console.log(updatedUser[n], __filename);
+		userToken.should.not.equal(lastToken);
+		let password = updateUserPassword[n++];
+		password.should.not.deep.equal(lastPassword);
+		const response = await chai
+			.request(application)
+			.patch('/api/v1/user/password')
+			.send(password)
+			.auth(userToken, { type: 'bearer' });
+		response.should.have.status(StatusCodes.NO_CONTENT);
+		lastToken = userToken;
+		lastPassword = password;
+	}
+}
+
+let getDeletedUser = async () => {
+	const userTokens: string[] = await users.getUserTokens();
+	userTokens.should.not.be.empty;
+	for (const userToken of userTokens) {
+		const response = await chai
+			.request(application)
+			.get('/api/v1/user')
+			.auth(userToken, { type: 'bearer' });
+		response.should.have.status(StatusCodes.NOT_FOUND);
+	}
+};
+
+async function deleteUser() {
+	const userTokens: string[] = await users.getUserTokens();
+	userTokens.should.not.be.empty;
+	for (const userToken of userTokens) {
+		const response = await chai
+			.request(application)
+			.delete('/api/v1/user')
+			.auth(userToken, { type: 'bearer' });
+		response.should.have.status(StatusCodes.OK);
+	}
+}
+
+export { getUser, patchUser, patchUserPassword, getDeletedUser, deleteUser };

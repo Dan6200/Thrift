a32616df849e722cb5a676d18d17e6c634cb1790
Daniel Nyong <danielnyong66@gmail.com>
2022-09-25 00:38:41 +0100

    updated imports

--- server/src/tests/helpers/index.ts
+++ server/src/tests/helpers/index.ts
@@ -1,11 +1,13 @@
 import registration from './registration';
 import login from './login';
-import getUser from './get-user';
-import getDeletedUser from './get-deleted-user';
-import patchUser from './patch-user';
-import patchUserPassword from './patch-user-password';
-import deleteUser from './delete-user';
 import createShipping from './create-shipping';
+import {
+	deleteUser,
+	getDeletedUser,
+	getUser,
+	patchUser,
+	patchUserPassword,
+} from './user';
 
 export {
 	registration,

62bdae60c0047aab0858f28ce1d117e282851a7a
Daniel Nyong <danielnyong66@gmail.com>
2022-10-26 07:49:28 +0100

    Finished with the customer route and route processor helper

--- server/src/controllers/customer-account/index.ts
+++ server/src/controllers/customer-account/index.ts
@@ -2,8 +2,11 @@ import processRoute from 'controllers/helpers/process-route';
 import db from 'db';
 import { StatusCodes } from 'http-status-codes';
 const { CREATED, OK, NO_CONTENT, NOT_FOUND } = StatusCodes;
+
+type Status = typeof CREATED | typeof OK | typeof NO_CONTENT | typeof NOT_FOUND;
+
 type responseData = {
-	status: typeof CREATED | typeof OK | typeof NO_CONTENT | typeof NOT_FOUND;
+	status: Status;
 	data?: string | object;
 };
 
@@ -15,14 +18,14 @@ const createQuery = [
 			db.query(`select * from customer where customer_id=$1`, [userId]),
 	],
 	deleteQuery = [() => db.query(`delete from customer`)],
-	validateResult = (result: any): responseData => {
+	validateResult = (result: any, status: Status): responseData => {
 		if (result.rowCount === 0)
 			return {
 				status: 404,
 				data: 'Route does not exit',
 			};
 		return {
-			status: 200,
+			status,
 			data: result.rows[result.rowCount - 1],
 		};
 	};
@@ -37,8 +40,7 @@ let createCustomerAccount = processRoute(createQuery, { status: CREATED }),
 	deleteCustomerAccount = processRoute(
 		deleteQuery,
 		{ status: NO_CONTENT },
-		undefined,
-		validateResult
+		undefined
 	);
 
 export { createCustomerAccount, getCustomerAccount, deleteCustomerAccount };
--- server/src/controllers/helpers/process-route.ts
+++ server/src/controllers/helpers/process-route.ts
@@ -6,50 +6,55 @@ import path from 'path';
 
 // TODO: scrutinize this function
 const { CREATED, OK, NO_CONTENT, NOT_FOUND } = StatusCodes;
-type ResponseMsg = {
-	status: typeof CREATED | typeof OK | typeof NO_CONTENT | typeof NOT_FOUND;
+
+type Status = typeof CREATED | typeof OK | typeof NO_CONTENT | typeof NOT_FOUND;
+
+type ResponseData = {
+	status: Status;
 	data?: string | object;
 };
 export default (
 	dbQueries: ((sqlData: object) => any)[],
-	responseMsg: ResponseMsg,
+	responseData: ResponseData,
 	validateBody?: (data: object) => any,
-	validateResult?: (data: object) => ResponseMsg,
+	validateResult?: (data: object, status: Status) => ResponseData,
 	processData?: (data: object) => any
 ) => {
 	// return the route processor middleware
 	return async (request: RequestWithPayload, response: Response) => {
 		// variables
 		let result: any,
-			{ userId } = request.user,
-			status: number,
-			data: object | string | undefined;
+			reqData: any,
+			{ userId } = request.user;
 		// set status code and response data
 		// Validate request data
 		if (request.body && validateBody) {
 			// validateBody returns error status code and message if body is invalid
-			const { status, data } = validateBody(request.body);
+			const { status, reqData } = validateBody(request.body);
 			// check for errors
-			if (status >= 400) return response.status(status).send(data);
+			if (status >= 400) return response.status(status).send(reqData);
 		}
 		// Process the requestData
-		if (processData && data) data = processData(data as object);
+		if (processData && reqData) reqData = processData(reqData as object);
+		let { status, data } = responseData;
 		for (let query of dbQueries) {
 			result = await query({
 				userId,
 				params: request.params,
-				data,
+				reqData,
 			});
 			debugger;
 			if (result && validateResult) {
 				// validateBody returns error status code and message if data is invalid
-				({ status, data } = validateResult(result));
+				({ status, data } = validateResult(
+					result,
+					responseData.status
+				));
 				// check for errors
 				if (status >= 400) return response.status(status).send(data);
 			}
 		}
 		// Update the data with the same status code
-		// TODO:choose a better name for this
-		response.status(responseMsg.status).send(data);
+		response.status(status).send(data);
 	};
 };

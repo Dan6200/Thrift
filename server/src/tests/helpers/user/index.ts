84d6d1ce685e46290e81f86b4585065908eb20fc
Daniel Nyong <danielnyong66@gmail.com>
2022-11-18 20:39:51 +0100

    Finished refactoring user route test helpers

--- server/src/tests/helpers/user/index.ts
+++ server/src/tests/helpers/user/index.ts
@@ -4,16 +4,12 @@ import { UserDataSchemaDB } from 'app-schema/users';
 import application from 'application';
 import { StatusCodes } from 'http-status-codes';
 import Joi from 'joi';
-import {
-	updateUser,
-	updateUserPassword,
-	userDataTesting,
-} from 'tests/authentication/user-data';
+import { updateUser, updateUserPassword } from 'tests/authentication/user-data';
 import testProcessRoute from '../test-process-route';
 
 chai.use(chaiHttp).should();
 
-const { CREATED, OK, NOT_FOUND } = StatusCodes;
+const { OK, NOT_FOUND, NO_CONTENT } = StatusCodes;
 
 let validateResult = (data: any) => {
 	let userInfo = data;
@@ -42,86 +38,28 @@ const testUpdateUser = testProcessRoute({
 
 const testUpdateUserPassword = testProcessRoute({
 	verb: 'patch',
-	statusCode: OK,
-	dataList: updateUser,
+	statusCode: NO_CONTENT,
+	dataList: updateUserPassword,
 	...routeParams,
 	baseUrl: routeParams.baseUrl + '/password',
 });
 
-const testUpdateUser = testProcessRoute({
-	verb: 'patch',
+const testDeleteUser = testProcessRoute({
+	verb: 'delete',
 	statusCode: OK,
-	dataList: updateUser,
 	...routeParams,
 });
 
-async function GetUser() {
-	userTokens.should.not.be.empty;
-	for (const userToken of userTokens) {
-		const response = await chai
-			.request(application)
-			.get('/api/v1/user')
-			.auth(userToken, { type: 'bearer' });
-		response.should.have.status(StatusCodes.OK);
-		response.body.should.be.an('object');
-		Joi.assert(response.body, UserDataSchemaDB);
-	}
-}
+const testGetNonExistentUser = testProcessRoute({
+	verb: 'get',
+	statusCode: NOT_FOUND,
+	...routeParams,
+});
 
-async function patchUser() {
-	let n = 0;
-	const userTokens: string[] = await userDataTesting.get('tokens');
-	userTokens.should.not.be.empty;
-	for (const userToken of userTokens) {
-		const response = await chai
-			.request(application)
-			.patch('/api/v1/user/info')
-			.send(updateUser[n])
-			.auth(userToken, { type: 'bearer' });
-		response.should.have.status(StatusCodes.OK);
-		response.body.should.be.an('object');
-		Joi.assert(response.body, UserDataSchemaDB);
-		n++;
-	}
-}
-
-async function patchUserPassword() {
-	let n = 0;
-	const userTokens: string[] = await userDataTesting.get('tokens');
-	userTokens.should.not.be.empty;
-	for (const userToken of userTokens) {
-		let password = updateUserPassword[n++];
-		const response = await chai
-			.request(application)
-			.patch('/api/v1/user/password')
-			.send(password)
-			.auth(userToken, { type: 'bearer' });
-		response.should.have.status(StatusCodes.NO_CONTENT);
-	}
-}
-
-let getDeletedUser = async () => {
-	const userTokens: string[] = await userDataTesting.get('tokens');
-	userTokens.should.not.be.empty;
-	for (const userToken of userTokens) {
-		const response = await chai
-			.request(application)
-			.get('/api/v1/user')
-			.auth(userToken, { type: 'bearer' });
-		response.should.have.status(StatusCodes.NOT_FOUND);
-	}
+export {
+	testGetUser,
+	testUpdateUser,
+	testUpdateUserPassword,
+	testDeleteUser,
+	testGetNonExistentUser,
 };
-
-async function deleteUser() {
-	const userTokens: string[] = await userDataTesting.get('tokens');
-	userTokens.should.not.be.empty;
-	for (const userToken of userTokens) {
-		const response = await chai
-			.request(application)
-			.delete('/api/v1/user')
-			.auth(userToken, { type: 'bearer' });
-		response.should.have.status(StatusCodes.OK);
-	}
-}
-
-export { testGetUser, testUpdateUser };

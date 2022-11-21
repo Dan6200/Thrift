238226e2a17dd8cf2ddec732c9e9018f5dd14fcc
Daniel Nyong <danielnyong66@gmail.com>
2022-11-21 05:49:31 +0100

    Editor (Vim) auto refactor

--- server/src/middleware/error-handler.ts
+++ server/src/middleware/error-handler.ts
@@ -1,12 +1,23 @@
 import { StatusCodes } from 'http-status-codes';
 
-const errorHandlerMiddleware = async (err, req, res, next) => {
+const errorHandlerMiddleware = async (
+	err: { statusCode: any; message: any },
+	_req: any,
+	res: {
+		status: (arg0: any) => {
+			(): any;
+			new (): any;
+			send: { (arg0: string): void; new (): any };
+			json: { (arg0: { msg: any }): any; new (): any };
+		};
+	},
+	_next: any
+) => {
 	let customError = {
 		// set default
 		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
 		msg: err.message || 'Something went wrong try again later',
 	};
-	console.log(err);
 	if (customError.statusCode === StatusCodes.NOT_FOUND)
 		res.status(customError.statusCode).send(
 			`<h1>

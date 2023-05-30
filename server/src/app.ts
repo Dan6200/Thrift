bc4909e60c3c1db3251e5c62f3c30ba7e93489c9
Daniel Nyong <danielnyong66@gmail.com>
2023-05-30 10:46:32 +0100

    Uncommented the logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -39,7 +39,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 // routes
 // public
 app.use("/api/v1/auth", authRouter);

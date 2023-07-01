96afc0fd3b164065566c4f32ddd2611af17a3c03
Daniel Nyong <danielnyong66@gmail.com>
2023-07-01 22:52:55 +0100

    Changed Userdata schema

--- server/src/app-schema/account.ts
+++ server/src/app-schema/account.ts
@@ -3,7 +3,8 @@
 import joi from 'joi'
 
 const AccountDataSchemaRequest = joi
-	.object({
+	.object()
+	.keys({
 		first_name: joi.string().alphanum().min(3).max(30).required(),
 		last_name: joi.string().alphanum().min(3).max(30).required(),
 		email: joi
@@ -11,21 +12,23 @@ const AccountDataSchemaRequest = joi
 			.pattern(
 				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 			)
-			.required(),
+			.allow(null),
 		phone: joi
 			.string()
 			.pattern(
 				/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/
 			)
-			.required(),
+			.allow(null),
 		password: joi.string().required(),
 		dob: joi.date().required(),
 		country: joi.string().required(),
 	})
+	.or('email', 'phone')
 	.required()
 
 const AccountDataSchemaDB = joi
-	.object({
+	.object()
+	.keys({
 		first_name: joi.string().alphanum().min(3).max(30).required(),
 		last_name: joi.string().alphanum().min(3).max(30).required(),
 		email: joi
@@ -33,18 +36,19 @@ const AccountDataSchemaDB = joi
 			.pattern(
 				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 			)
-			.required(),
+			.allow(null),
 		phone: joi
 			.string()
 			.pattern(
 				/^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/
 			)
-			.required(),
+			.allow(null),
 		dob: joi.alternatives().try(joi.date().required(), joi.string().required()),
 		country: joi.string().required(),
 		is_customer: joi.boolean().required(),
 		is_vendor: joi.boolean().required(),
 	})
+	.or('email', 'phone')
 	.required()
 
 export { AccountDataSchemaRequest, AccountDataSchemaDB }
--- server/src/db/sql-scripts/create-table.sql
+++ server/src/db/sql-scripts/create-table.sql
@@ -4,13 +4,16 @@ create schema if not exists public;
 create table if not exists user_accounts (
   user_id      serial                    primary    key,
   first_name   varchar(30)               not        null,
+	check				(first_name ~* '^[a-zA-Z]+$'),
   last_name    varchar(30)               not        null,
+	check				(last_name ~* '^[a-zA-Z]+$'),
   email        varchar(320)              unique
   check        (email ~* '^(([^<> ()[\]\\.,;:\s@"]+(\.[^< > ()[\]\\.,;'
 							 ':\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1'
 							 ',3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'),  
   phone        varchar(40)               unique
   check        (phone ~* '^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$'),  
+	check				 (email is null and phone is not null or email is not null and phone is null),
   password     bytea                     not        null,
   dob          date                      not        null,
   country      varchar                   not        null,

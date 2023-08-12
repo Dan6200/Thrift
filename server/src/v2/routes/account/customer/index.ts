5438a3d9b2e770dace636e421699ec272206f5ec
Daniel Nyong <danielnyong66@gmail.com>
2023-08-12 03:15:37 +0100

    Removed v2 folder

--- server/src/v2/routes/account/customer/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {
-  createCustomerAccount,
-  deleteCustomerAccount,
-} from '../../../controllers/account/customer/index.js'
-const router = express.Router()
-
-router.route('/').post(createCustomerAccount).delete(deleteCustomerAccount)
-export default router

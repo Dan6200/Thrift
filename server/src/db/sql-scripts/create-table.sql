f52aa164a9a745efdd18d6122b569fb0e5f4f1c8
Daniel Nyong <danielnyong66@gmail.com>
2023-08-15 21:11:32 +0100

    Updated db tables

--- server/src/db/sql-scripts/create-table.sql
+++ server/src/db/sql-scripts/create-table.sql
@@ -18,19 +18,19 @@ create table if not exists user_accounts (
 							 or email is not null and phone is null),
   password     bytea                     not        null,
   dob          date                      not        null,
-  country      varchar                   not        null,
+  country      varchar                   not        null 			default  		'Nigeria',
   check        (current_date - dob > 12)
 );
 
 
 create table if not exists customers (
-  customer_id   int   primary key   references   user_accounts   on   delete   cascade
+  customer_id   int   primary key   references   user_accounts   on   delete   restrict
 );
 
 
 create table if not exists shipping_info (
   shipping_info_id        serial        primary   key,
-  customer_id             int           not       null    references   customers   on   delete   cascade,
+  customer_id             int           not       null    references   customers   on   delete   restrict,
   recipient_first_name    varchar(30)   not       null,
   recipient_last_name     varchar(30)   not       null,
   address                 varchar       not       null,
@@ -48,14 +48,14 @@ create table if not exists payment_info (
 
 
 create table if not exists vendors (
-  vendor_id   int   primary key   references   user_accounts   on   delete   cascade
+  vendor_id   int   primary key   references   user_accounts   on   delete   restrict
 );
 
 
 create table if not exists stores (
   store_id       serial    primary   key,   
   store_name     varchar   not       null,
-  vendor_id      int       not       null    references   vendors        on   delete   cascade,
+  vendor_id      int       not       null    references   vendors        on   delete   restrict,
   store_page     jsonb,
   date_created   date      not       null    default      current_date
 );
@@ -69,14 +69,14 @@ create table if not exists products (
   list_price           numeric(19,4),
   net_price            numeric(19,4),
   vendor_id            int              not       null,
-  store_id             int              not       null    references   stores          on   delete   cascade,
+  store_id             int              not       null    references   stores          on   delete   restrict,
   created_at           timestamptz      not       null    default      now(),
   quantity_available   int              not       null
 );
 
 
 create table if not exists product_media (
-  product_id    int       not null   references   products   on   delete   cascade,
+  product_id    int       not null   references   products   on   delete   restrict,
   filename      varchar   primary    key,
   filepath      varchar   not        null,
   description   varchar
@@ -89,7 +89,7 @@ create table if not exists product_display_image (
 
 create table if not exists shopping_cart (
   cart_id       serial        primary   key,
-  customer_id   int           not       null   references   customers   on   delete   cascade,
+  customer_id   int           not       null   references   customers   on   delete   restrict,
   created       timestamptz   not       null   default      now(),
   updated       timestamptz   not       null   default      now()
 );
@@ -97,8 +97,8 @@ create table if not exists shopping_cart (
 
 create table if not exists shopping_cart_item (
   item_id      serial   primary   key,
-  cart_id      int      not       null   references   shopping_cart   on   delete   cascade,
-  product_id   int      not       null   references   products        on   delete   cascade,
+  cart_id      int      not       null   references   shopping_cart   on   delete   restrict,
+  product_id   int      not       null   references   products        on   delete   restrict,
   quantity     int      not       null   check        (quantity > 0)
 );
 
@@ -116,8 +116,8 @@ create table if not exists transaction_details (
 
 create table if not exists purchases (
   item_id          serial        primary   key,
-  product_id       int           not       null   references   products              on        delete   cascade,
-  transaction_id   int           not       null   references   transaction_details   on        delete   cascade,
+  product_id       int           not       null   references   products              on        delete   restrict,
+  transaction_id   int           not       null   references   transaction_details   on        delete   restrict,
   created          timestamptz   not       null   default      now()                 unique,
   updated          timestamptz   not       null   default      now()                 unique,
   quantity         int           not       null   check        (quantity > 0)
@@ -125,27 +125,27 @@ create table if not exists purchases (
 
 
 create table if not exists product_reviews (
-  product_id        int            primary   key     references   products              on   delete   cascade,
-  transaction_id    int            not       null    references   transaction_details   on   delete   cascade,
+  product_id        int            primary   key     references   products              on   delete   restrict,
+  transaction_id    int            not       null    references   transaction_details   on   delete   restrict,
   rating            numeric(3,2)   not       null,
-  customer_id       int            not       null    references   customers             on   delete   cascade,
+  customer_id       int            not       null    references   customers             on   delete   restrict,
   customer_remark   varchar
 );
 
 
 create table if not exists vendor_reviews (
-  vendor_id         int            primary   key     references   vendors               on   delete   cascade,
-  customer_id       int            not       null    references   customers             on   delete   cascade,
-  transaction_id    int            not       null    references   transaction_details   on   delete   cascade,
+  vendor_id         int            primary   key     references   vendors               on   delete   restrict,
+  customer_id       int            not       null    references   customers             on   delete   restrict,
+  transaction_id    int            not       null    references   transaction_details   on   delete   restrict,
   rating            numeric(3,2)   not       null,
   customer_remark   varchar
 );
 
 
 create table if not exists customer_reviews (
-  customer_id      int            primary   key     references   customers             on   delete   cascade,
-  vendor_id        int            not       null    references   vendors               on   delete   cascade,
-  transaction_id   int            not       null    references   transaction_details   on   delete   cascade,
+  customer_id      int            primary   key     references   customers             on   delete   restrict,
+  vendor_id        int            not       null    references   vendors               on   delete   restrict,
+  transaction_id   int            not       null    references   transaction_details   on   delete   restrict,
   rating           numeric(3,2)   not       null,
   vendor_remark    varchar
 );

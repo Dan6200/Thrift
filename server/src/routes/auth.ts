5ab1f000edc449966038a903fe8f58ca6ec3c820
Daniel Nyong <danielnyong66@gmail.com>
2023-08-18 15:16:40 +0100

    re-added authentication for logout

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,10 +2,11 @@ import { Router } from 'express'
 import login from '../controllers/auth/login.js'
 import logout from '../controllers/auth/logout.js'
 import register from '../controllers/auth/register.js'
+import authentication from '../middleware/authentication.js'
 const handleAuth = Router()
 
 handleAuth.post('/login', login)
 handleAuth.post('/register', register)
-handleAuth.delete('/logout', logout)
+handleAuth.delete('/logout', authentication, logout)
 
 export default handleAuth

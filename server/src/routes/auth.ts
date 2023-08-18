8c6dc1d47b422eb449751911738ee16aaa0318fc
Daniel Nyong <danielnyong66@gmail.com>
2023-08-18 14:06:33 +0100

    removed unnecessary auth middleware

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,11 +2,10 @@ import { Router } from 'express'
 import login from '../controllers/auth/login.js'
 import logout from '../controllers/auth/logout.js'
 import register from '../controllers/auth/register.js'
-import authentication from '../middleware/authentication.js'
 const handleAuth = Router()
 
 handleAuth.post('/login', login)
 handleAuth.post('/register', register)
-handleAuth.delete('/logout', authentication, logout)
+handleAuth.delete('/logout', logout)
 
 export default handleAuth

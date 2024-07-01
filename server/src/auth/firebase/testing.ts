4b8afb49a6b0127c521bb36ae0a7d35a311088b2
Daniel Nyong <danielnyong66@gmail.com>
2024-07-01 09:17:09 +0100

    debugging

--- server/src/auth/firebase/testing.ts
+++ server/src/auth/firebase/testing.ts
@@ -3,7 +3,14 @@ import { initializeApp } from 'firebase/app'
 
 let firebaseConfig = {}
 if (process.env.NODE_ENV === 'testing') {
-  firebaseConfig = process.env.FB_CONFIG
+  firebaseConfig = {
+    apiKey: 'AIzaSyCw_JRQCsUEJeNIywcwP8n6-9j3t5cMb6s',
+    authDomain: 'thrift-app-fb.firebaseapp.com',
+    projectId: 'thrift-app-fb',
+    storageBucket: 'thrift-app-fb.appspot.com',
+    messagingSenderId: '393857060713',
+    appId: '1:393857060713:web:cddd0ee1013735d159b7f8',
+  }
 } else throw new Error('Not available during production or development')
 console.log('firebase config: ', firebaseConfig)
 const app = initializeApp(firebaseConfig)

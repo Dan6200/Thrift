799d5a363cfab89f3d1fa5cf4451cfeda17f9ad8
Daniel Nyong <danielnyong66@gmail.com>
2024-07-01 09:48:10 +0100

    parsing the config env file

--- server/src/auth/firebase/testing.ts
+++ server/src/auth/firebase/testing.ts
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app'
 
 let firebaseConfig = {}
 if (process.env.NODE_ENV === 'testing') {
-  firebaseConfig = process.env.FB_CONFIG
+  firebaseConfig = JSON.parse(process.env.FB_CONFIG)
 } else throw new Error('Not available during production or development')
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)

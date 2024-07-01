1a60e508bdeae8ba1caef2eb052dd22dde7ce9b3
Daniel Nyong <danielnyong66@gmail.com>
2024-07-01 09:21:26 +0100

    removed env vars

--- server/src/auth/firebase/testing.ts
+++ server/src/auth/firebase/testing.ts
@@ -3,15 +3,7 @@ import { initializeApp } from 'firebase/app'
 
 let firebaseConfig = {}
 if (process.env.NODE_ENV === 'testing') {
-  firebaseConfig = {
-    apiKey: 'AIzaSyCw_JRQCsUEJeNIywcwP8n6-9j3t5cMb6s',
-    authDomain: 'thrift-app-fb.firebaseapp.com',
-    projectId: 'thrift-app-fb',
-    storageBucket: 'thrift-app-fb.appspot.com',
-    messagingSenderId: '393857060713',
-    appId: '1:393857060713:web:cddd0ee1013735d159b7f8',
-  }
+  firebaseConfig = process.env.FB_CONFIG
 } else throw new Error('Not available during production or development')
-console.log('firebase config: ', firebaseConfig)
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)

3a5bb1d0644a4fdd08ff8ef9c12b85dc431e628c
Daniel Nyong <danielnyong66@gmail.com>
2024-07-02 10:14:36 +0100

    fixing sql query string

--- server/src/controllers/user/utils.ts
+++ server/src/controllers/user/utils.ts
@@ -4,7 +4,7 @@ SELECT ua.first_name, ua.last_name, ua.email, ua.phone, ua.country, ua.dob
 			 CASE WHEN v.vendor_id IS NOT NULL THEN TRUE ELSE FALSE END AS is_vendor
 FROM user_accounts ua
 LEFT JOIN customers c
-ON ua.user_id = c.customer_id
+ON ua.uid = c.customer_id
 LEFT JOIN vendors v
-ON ua.user_id = v.vendor_id
-WHERE ua.user_id = $1`
+ON ua.uid = v.vendor_id
+WHERE ua.uid = $1`

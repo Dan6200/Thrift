c049d5bcbf0de3a38c6170541a51c048091238e6
Daniel Nyong <danielnyong66@gmail.com>
2024-07-02 10:47:09 +0100

    fixed sql query string

--- server/src/controllers/user/utils.ts
+++ server/src/controllers/user/utils.ts
@@ -1,10 +1,10 @@
 export const getUserQueryString = `
-SELECT ua.first_name, ua.last_name, ua.email, ua.phone, ua.country, ua.dob,
-			 CASE WHEN c.customer_id IS NOT NULL THEN TRUE ELSE FALSE END AS is_customer,
+SELECT first_name, last_name, email, phone, country, dob,
+			 CASE WHEN customer_id IS NOT NULL THEN TRUE ELSE FALSE END AS is_customer,
 			 CASE WHEN v.vendor_id IS NOT NULL THEN TRUE ELSE FALSE END AS is_vendor
-FROM user_accounts ua
-LEFT JOIN customers c
-ON ua.uid = c.customer_id
-LEFT JOIN vendors v
-ON ua.uid = v.vendor_id
-WHERE ua.uid = $1`
+FROM users
+LEFT JOIN customers
+ON uid = customer_id
+LEFT JOIN vendors
+ON uid = vendor_id
+WHERE uid = $1`

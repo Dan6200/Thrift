36940a756372c0754195938e78b903ec8d7274bb
Daniel Nyong <danielnyong66@gmail.com>
2024-07-02 10:50:32 +0100

    updated sql query

--- server/src/controllers/user/utils.ts
+++ server/src/controllers/user/utils.ts
@@ -1,7 +1,7 @@
 export const getUserQueryString = `
 SELECT first_name, last_name, email, phone, country, dob,
 			 CASE WHEN customer_id IS NOT NULL THEN TRUE ELSE FALSE END AS is_customer,
-			 CASE WHEN v.vendor_id IS NOT NULL THEN TRUE ELSE FALSE END AS is_vendor
+			 CASE WHEN vendor_id IS NOT NULL THEN TRUE ELSE FALSE END AS is_vendor
 FROM users
 LEFT JOIN customers
 ON uid = customer_id

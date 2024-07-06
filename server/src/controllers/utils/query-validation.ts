a86580dba1e4e858657b67ced3880708f4ff3a4c
Daniel Nyong <danielnyong66@gmail.com>
2024-07-06 09:09:36 +0100

    working on post /shipping-info

--- server/src/controllers/utils/query-validation.ts
+++ server/src/controllers/utils/query-validation.ts
@@ -10,7 +10,7 @@ import { isTypeQueryResultRow } from '../../types-and-interfaces/response.js'
 export const validateReqData =
   <T>(schema: ObjectSchema<T>) =>
   (data: unknown) => {
-    if (typeof data == 'undefined' || Object.keys(data).length === 0)
+    if (typeof data == 'undefined' || Object.keys(data)?.length === 0)
       throw new BadRequestError('request data cannot be empty')
     const { error } = schema.validate(data)
     if (error) throw new BadRequestError(error.message)

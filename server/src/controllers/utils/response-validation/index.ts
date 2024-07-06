e62d75dc2dfb54b09f2eab2b42cd08cc84856b69
Daniel Nyong <danielnyong66@gmail.com>
2024-07-06 15:15:32 +0100

    debugging

--- server/src/controllers/utils/response-validation/index.ts
+++ server/src/controllers/utils/response-validation/index.ts
@@ -33,7 +33,7 @@ export function validateResData<T>(schema: ArraySchema<T> | ObjectSchema<T>) {
       } else return false
       return true
     } else {
-      console.dir(result)
+      console.log('shipping info result', result)
       if (result.length > 1) {
         const { error } = schema.validate(result)
         if (error) throw new BadRequestError(error.message)

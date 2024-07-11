c43628ec9e0e86cfc614f9a416b3571ec5cfb66b
Daniel Nyong <danielnyong66@gmail.com>
2024-07-11 16:06:07 +0100

    fixed import bug

--- server/src/auth/firebase/index.ts
+++ server/src/auth/firebase/index.ts
@@ -7,12 +7,15 @@ dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
 const { credential } = fbAdmin
 let app = null
 let serviceAccount: any = null
-serviceAccount = await import(process.env.FB_SERVICE_ACCOUNT, {
-  assert: { type: 'json' },
-}).then((module) => {
-  serviceAccount = module.default
-  return serviceAccount
-})
+if (process.env.NODE_ENV === 'production')
+  serviceAccount = await import(process.env.FB_SERVICE_ACCOUNT, {
+    assert: { type: 'json' },
+  }).then((module) => {
+    serviceAccount = module.default
+    return serviceAccount
+  })
+if (process.env.NODE_ENV === 'testing')
+  serviceAccount = JSON.parse(process.env.FB_SERVICE_ACCOUNT)
 
 app = initializeApp({
   credential: credential.cert(serviceAccount),

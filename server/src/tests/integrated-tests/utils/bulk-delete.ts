fdd7193860f76a6b3f229ffe30cf376a0afb9f51
Daniel Nyong <danielnyong66@gmail.com>
2024-07-12 12:40:37 +0100

    fixing the bulk delete function

--- server/src/tests/integrated-tests/utils/bulk-delete.ts
+++ server/src/tests/integrated-tests/utils/bulk-delete.ts
@@ -3,21 +3,24 @@
 import { cloudinary } from '@/controllers/utils/media-storage.js'
 
 export async function bulkDeleteImages() {
-  try {
-    const { resources } = await cloudinary.api.resources({
-      type: 'upload',
-      prefix: 'thrift-app-media',
-      max_results: 500,
-    })
-    const publicIds = resources.map((resource: any) => resource.public_id)
-    if (publicIds.length > 0) {
-      const result = await cloudinary.api.delete_resources(publicIds)
-      console.log('Deleted assets: ', result)
-    } else {
-      console.log('No assets found in the folder')
+  while (true) {
+    try {
+      const { resources } = await cloudinary.api.resources({
+        type: 'upload',
+        prefix: 'thrift-app-media',
+        max_results: 100,
+      })
+      const publicIds = resources.map((resource: any) => resource.public_id)
+      if (publicIds.length > 0) {
+        const result = await cloudinary.api.delete_resources(publicIds)
+        console.log('Deleted assets: ', result)
+      } else {
+        console.log('No assets found in the folder')
+        break
+      }
+    } catch (err) {
+      console.error('Error deleting assets: ', err)
     }
-  } catch (err) {
-    console.error('Error deleting assets: ', err)
   }
 }
 bulkDeleteImages()

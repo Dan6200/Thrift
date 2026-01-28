f3298d9099ba53b3d43241db956871dd4e249419
Daniel Nyong <danielnyong66@gmail.com>
2026-01-28 20:22:00 +0100

    switched to upstash redis

--- packages/thrift-api/server/src/db/redis/index.ts
+++ packages/thrift-api/server/src/db/redis/index.ts
@@ -1,36 +1,35 @@
-// import redis from 'redis'
-//
-// const client = redis.createClient({
-//   url: process.env.REDIS_URL,
-// })
-//
-// client.on('error', (err) => {
-//   console.log('Redis Server Error...')
-//   console.error(err)
-// })
-//
-// export default {
-//   async connect() {
-//     if (client.isOpen) return
-//     await client.connect()
-//   },
-//   async quit() {
-//     if (client.isOpen) client.quit()
-//   },
-//   async getClient() {
-//     this.connect()
-//     return client
-//   },
-//   async sAdd(key: string, value: string) {
-//     this.connect()
-//     const retVal = await client.sAdd(key, value)
-//     // this.quit()
-//     return retVal
-//   },
-//   async sIsMember(key: string, value: string) {
-//     this.connect()
-//     const retVal = await client.sIsMember(key, value)
-//     // this.quit()
-//     return retVal
-//   },
-// }
+import { Redis } from '@upstash/redis'
+
+export const client = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+})
+
+/* We may need to switch back to node-redis...
+ *
+export default {
+  async connect() {
+    if (client.isOpen) return
+    await client.connect()
+  },
+  async quit() {
+    if (client.isOpen) client.quit()
+  },
+  async getClient() {
+    this.connect()
+    return client
+  },
+  async sAdd(key: string, value: string) {
+    this.connect()
+    const retVal = await client.sAdd(key, value)
+    // this.quit()
+    return retVal
+  },
+  async sIsMember(key: string, value: string) {
+    this.connect()
+    const retVal = await client.sIsMember(key, value)
+    // this.quit()
+    return retVal
+  },
+}
+*/
